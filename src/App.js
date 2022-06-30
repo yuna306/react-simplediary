4b6677a7fa3d928311b94a32ed14cc1a16318143
yuna306 <happy.yuna306@gmail.com>
2022-06-30 17:01:02 +0900

    리액트의 라이프 사이클과 useEffect

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import "./App.css";
 import { useRef, useState } from "react";
+import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  /* react에서 API 호출하기 
+  https://jsonplaceholder.typicode.com/comments 의 데이터를 가져와서
+  보여주는 역할을 하는 함수 
+  */
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -41,6 +47,7 @@ function App() {
   return (
     <div className="App">
       <h2>일기장</h2>
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} diaryList={data} onRemove={onRemove} />
     </div>
---
+++ src/Lifecycle.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const Lifecycle = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  /*
+마운트 된 시점에 무언가 작업을 하고 싶다면 콜백함수를 이용!
+dependency array는 빈배열로 둠
+ */
+
+  useEffect(() => {
+    console.log("Mount!");
+  }, []);
+
+  /*
+업데이트되는 순간
+state를 바꾸는순간 (setCount로 카운트를 증가시키는 순간 )에 하고싶은일이 있다면
+dependency array를 전달하지 않음
+ */
+
+  useEffect(() => {
+    console.log("update!");
+  });
+
+  /*dependency array에 있는 값이 변경되는순간 
+useEffect의 콜백함수가 실행 됨  */
+  useEffect(() => {
+    console.log(`count is update : ${text}`);
+  }, [count]);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <div>
+        {count}
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+    </div>
+  );
+};
+
+export default Lifecycle;
